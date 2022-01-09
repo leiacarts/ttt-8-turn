264c94b1a74b20ac0f124e523e62589ff3326444
Leia Carter <leiacarts@gmail.com>
2022-01-09 05:32:09 -0500

    done

---
+++ lib/turn.rb
@@ -0,0 +1,29 @@
+board = ["  ","  ","  ","  ","  ","  ","  ","  ","  "]
+
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  def move(board, position, play = "X")
+    board[position] = play
+  end
+
+def valid_move?(board, index)
+    index.between?(0, 8) && !position_taken?(board, index)
+end
+
+def position_taken?(board, index)
+    if (board[index] == " " || board[index] == "" || board[index] == nil)
+        false
+    elsif (board[index] == "X" || board[index] == "O")
+        true
+    end
+end
